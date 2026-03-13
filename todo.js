51c86e42d30c93ca62212d4e06815a31dd5b4072
고민재 <minjae_koh@snu.ac.kr>
2026-03-13 12:00:41 +0900

    javascript 파일 연결
    입력칸, 추가버튼 활성화

---
+++ todo.js
@@ -0,0 +1,17 @@
+// html 파일에서 부품 가져오기
+const todoInput = document.querySelector('#todo-input'); // todo 입력칸 
+const addBtn = document.querySelector('#add-btn'); // 추가 버튼
+const todoList = document.querySelector('#todo-list'); // todo 바구니
+
+addBtn.addEventListener('click', function(){ // 버튼이 눌리면
+    const text = todoInput.value.trim(); // 사용자가 입력칸에 적은 글자를 text 변수에 담음
+
+    if (text === ''){
+        alert('할 일을 먼저 입력해주세요!');
+        todoInput.value = '';
+        todoInput.focus();
+        return;
+    }
+
+    alert('일정이 등록되었습니다!');
+})
