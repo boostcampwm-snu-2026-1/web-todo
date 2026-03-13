2e298e5c4e1886f987afe06e7c5e603a3e2dcb56
고민재 <minjae_koh@snu.ac.kr>
2026-03-13 12:10:16 +0900

    일정 추가 기능 구현

--- todo.js
+++ todo.js
@@ -13,5 +13,19 @@ addBtn.addEventListener('click', function(){ // 버튼이 눌리면
         return;
     }
 
-    alert('일정이 등록되었습니다!');
-})
+    // 일정 추가 기능 구현
+    const li = document.createElement('li');
+    li.className = 'todo-item';
+
+    li.innerHTML = `
+        <input type="checkbox" class="complete-checkbox">
+        <span class="todo-text">${text}</span>
+        <button class="delete-btn">삭제</button>
+        `;
+
+    todoList.appendChild(li);
+
+    todoInput.value = '';
+    todoInput.focus();
+});
+
