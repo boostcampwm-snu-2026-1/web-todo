661f2ab2e5b89579bbaef79af99f9ad17cfe08ab
고민재 <minjae_koh@snu.ac.kr>
2026-03-13 12:13:40 +0900

    엔터 입력기능 추가

--- todo.js
+++ todo.js
@@ -14,18 +14,24 @@ addBtn.addEventListener('click', function(){ // 버튼이 눌리면
     }
 
     // 일정 추가 기능 구현
-    const li = document.createElement('li');
+    const li = document.createElement('li'); // 브라우저에게 html에 없는 태그를 새로 만들어주라고 지시하는 역할
     li.className = 'todo-item';
 
-    li.innerHTML = `
+    li.innerHTML = ` 
         <input type="checkbox" class="complete-checkbox">
         <span class="todo-text">${text}</span>
         <button class="delete-btn">삭제</button>
-        `;
+        `; // 태그 안쪽의 내옹을 통째로 갈아끼우는 속성
 
     todoList.appendChild(li);
 
     todoInput.value = '';
     todoInput.focus();
+});
+
+todoInput.addEventListener('keypress', function(event){
+    if (event.key === 'Enter'){
+        addBtn.click();
+    }
 });
 
