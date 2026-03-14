8c7b74617b24c3b7b8176b36f3b985cba6435123
LEESEOKWOO <spssseok@snu.ac.kr>
2026-03-14 15:45:42 +0900

    create 함수 추가

--- todo.js
+++ todo.js
@@ -2,8 +2,28 @@ const todoValue = document.getElementById("todo-input"),
     listItems = document.getElementById("todo-list"),
     addUpdateClick = document.getElementById("AddUpdateClick");
     
+
+    todoValue.addEventListener("keypress", function(e){ // enter 누를 때도 list에 추가
+        if(e.key === "Enter"){
+            addUpdateClick.click();
+        }
+    });
+
 function CreateToDoData() {
-    alert(todoValue.value);
-    if(todoValue.value){
+    if(todoValue.value===""){
+        alert("Please Enter your todo text");
+        todoValue.focus(); // ?
     }
+
+    let li = document.createElement("li");
+        const todoItems = `<div title="Hit Double Click and Complete" ondblclick="CompletedToDoItems(this)">${todoValue.value}</div><div>
+                    <img class="edit todo-controls" onclick="UpdateToDoItems(this)" src="image/pencil.png" />
+                    <img class="delete todo-controls" onclick="DeleteToDoItems(this)" src="image/delete.png" /></div></div>`; // class의 어미 부분만 같아도 같은 클래스??
+    li.innerHTML = todoItems;
+    listItems.appendChild(li);
+    todoValue.value = "";
+
 }
+
+
+
