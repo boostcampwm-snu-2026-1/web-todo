712c08e04ab95a3498bf8adaf28ea3c4ce5206f2
LEESEOKWOO <spssseok@snu.ac.kr>
2026-03-14 13:17:37 +0900

    css 얼추 완료, js 추가

---
+++ todo.js
@@ -0,0 +1,9 @@
+const todoValue = document.getElementById("todo-input"),
+    listItems = document.getElementById("todo-list"),
+    addUpdateClick = document.getElementById("AddUpdateClick");
+    
+function CreateToDoData() {
+    alert(todoValue.value);
+    if(todoValue.value){
+    }
+}
