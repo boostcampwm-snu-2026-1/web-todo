a53b7f161160e091210a940fe5c7955a0564a0eb
myori <alinwinskingsleigh@gmail.com>
2026-03-17 17:29:56 +0900

    feat: renderTodos 함수 추가

--- todo.js
+++ todo.js
@@ -1,4 +1,19 @@
 
 
-const todo-list = require('fs');
+const todoList = [
+    { id: 1, text: "Do the laundry", completed: true },
+    { id: 2, text: "Buy groceries", completed: false },
+];
 
+
+function renderTodos () {
+    const todoContainer = document.querySelector(".task-container");
+    todoContainer.innerHTML = "";
+    todoList.forEach(todo => {
+        const todoItem = document.createElement("div");
+        todoItem.innerHTML = `<input type="checkbox" ${todo.completed ? "checked" : ""}><span class="task-text">${todo.text}</span><div><button type="button" class="edit-task-btn">🖋️</button><button type="button" class="delete-task-btn">🗑️</button></div>`;
+        todoContainer.appendChild(todoItem);
+    });
+};
+
+renderTodos();
