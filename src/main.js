3fb91988105cff502985131dd816d055919bb363
LEESEOKWOO <spssseok@snu.ac.kr>
2026-03-21 21:03:01 +0900

    fix : index.html line 42, add type = 'module'

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import {render, displayTodayDate} from "./render.js";
 let todoList = [];
 
 async function init(){
-    todoList = await getTodo();
-    render();
+    displayTodayDate();
+    console.log("data request...");
+
+
+    const data = await getTodo();
+    const todoContainer = document.querySelector('#todo-list');
+    render(data, todoContainer);
 }
-
-
 
 init();
