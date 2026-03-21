f4ceb372c7f8f070eecf71069bcaf13d3f866c87
YewonDanaLee <yennieday2@naver.com>
2026-03-21 17:00:23 +0900

    sprite svg로 수정

--- js/TodoView.js
+++ js/TodoView.js
@@ -33,10 +33,10 @@ export default class TodoView {
                 <span class="todo-text">${todo.content}</span>
                 <div class="action-btns">
                     <button class="icon-btn edit" data-action="edit">
-                        <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg>
+                        <svg width="18" height="18"><use href="#icon-edit"/></svg>
                     </button>
                     <button class="icon-btn delete" data-action="delete">
-                        <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
+                        <svg width="18" height="18"><use href="#icon-delete"/></svg>
                     </button>
                 </div>
             </li>
