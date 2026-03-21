dea09dcefce9067a200d0a00a8b4551b30226e56
JJ04s <bykim704@gmail.com>
2026-03-22 03:18:10 +0900

    할 일 추가 서버 연동 완료

--- layout/todo.js
+++ layout/todo.js
@@ -13,28 +13,20 @@ const day = days[today.getDay()];
 document.querySelector('#today-date').textContent = `${date} (${day})`;
 
 // 할 일 추가
+const API_URL = 'https://69bd31e32bc2a25b22add65b.mockapi.io/todos';
 const addBtn = document.querySelector('.add-task');
 const input = document.querySelector('.input-task');
 const taskContainer = document.querySelector('.container-box');
 let taskCounter = 1;
 
-addBtn.addEventListener('click', function(e) {
-    e.preventDefault();
-    const text = input.value;
-    let truncatedText = text;
+function renderTodoItem(todo) {
+    let truncatedText = todo.content;
     let titleAttribute = '';
 
-    if (text === '') {
-        document.querySelector('.error-empty').classList.add('show');
-        return;
+    if (todo.content.length > 15) {
+        truncatedText = todo.content.slice(0, 15) + "...";
+        titleAttribute = `title="${text}"`;
     }
-
-    else if (text.length > 15) {
-        truncatedText = truncatedText.slice(0, 15) + "...";
-        titleAttribute = `title="${text}"`
-    }
-
-    document.querySelector('.error-empty').classList.remove('show');
 
     const taskHTML = `
         <div class="div-task">
@@ -46,12 +38,67 @@ addBtn.addEventListener('click', function(e) {
                 <button type="button" class="delete-btn">삭제</button>
             </form>
         </div>
-    `;
+    `; 
 
     taskContainer.innerHTML += `${taskHTML}`;
+}
 
-    input.value = '';
-    taskCounter++;
+addBtn.addEventListener('click', async function(e) {
+    e.preventDefault();
+    const text = input.value;
+
+    if (text === '') {
+        document.querySelector('.error-empty').classList.add('show');
+        return;
+    }
+
+    // 서버에 POST
+    try {
+        const response = await fetch(API_URL, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                content: text,
+                completed: false
+            })
+        });
+
+        if (response.ok) {
+            const newTodo = await response.json();
+
+            renderTodoItem(newTodo);
+
+            input.value = '';
+            document.querySelector('.error-empty').classList.remove('show');
+        }
+    } catch (error) {
+        console.log("추가 실패:", error);
+        alert("서버 저장에 실패했습니다.");
+    }
+
+    // else if (text.length > 15) {
+    //     truncatedText = truncatedText.slice(0, 15) + "...";
+    //     titleAttribute = `title="${text}"`
+    // }
+
+    // document.querySelector('.error-empty').classList.remove('show');
+
+    // const taskHTML = `
+    //     <div class="div-task">
+    //         <form class="relative">
+    //             <label class="finish-task">
+    //                 <span class="checkmark"></span>
+    //             </label>
+    //             <p class="todo-task" ${titleAttribute}>${truncatedText}</p>
+    //             <button type="button" class="delete-btn">삭제</button>
+    //         </form>
+    //     </div>
+    // `;
+
+    // taskContainer.innerHTML += `${taskHTML}`;
+
+    // input.value = '';
+    // taskCounter++;
 });
 
 taskContainer.addEventListener('click', function(e) {
