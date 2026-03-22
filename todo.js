bbf6d17dfd0a96f8ff81282399c9b692ed8b375b
myori <alinwinskingsleigh@gmail.com>
2026-03-22 16:01:40 +0900

    fix: todoItem 태그를 li로 변경 및 task-item 클래스 추가

--- todo.js
+++ todo.js
@@ -10,8 +10,14 @@ function renderTodos () {
     const todoContainer = document.querySelector(".task-container");
     todoContainer.innerHTML = "";
     todoList.forEach(todo => {
-        const todoItem = document.createElement("div");
-        todoItem.innerHTML = `<input type="checkbox" ${todo.completed ? "checked" : ""}><span class="task-text">${todo.text}</span><div><button type="button" class="edit-task-btn">🖋️</button><button type="button" class="delete-task-btn">🗑️</button></div>`;
+        const todoItem = document.createElement("li");
+        todoItem.classList.add("task-item");
+        todoItem.innerHTML = `<input type="checkbox" ${todo.completed ? "checked" : ""}>
+        <span class="task-text">${todo.text}</span>
+        <div>
+            <button type="button" class="edit-task-btn">🖋️</button>
+            <button type="button" class="delete-task-btn">🗑️</button>
+        </div>`;
         todoContainer.appendChild(todoItem);
     });
 };
