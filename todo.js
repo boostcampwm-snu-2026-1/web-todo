5243bf2797ffbe53ea82374ce25cd90678125088
myori <alinwinskingsleigh@gmail.com>
2026-03-22 17:51:19 +0900

    feat: todo 추가 기능 구현 (버튼 클릭 및 엔터키 입력)

--- todo.js
+++ todo.js
@@ -4,22 +4,56 @@ const todoList = [
     { id: 1, text: "Do the laundry", completed: true },
     { id: 2, text: "Buy groceries", completed: false },
 ];
+const todoContainer = document.querySelector(".todo-container");
 
 
 function renderTodos () {
-    const todoContainer = document.querySelector(".task-container");
     todoContainer.innerHTML = "";
     todoList.forEach(todo => {
         const todoItem = document.createElement("li");
-        todoItem.classList.add("task-item");
+        todoItem.classList.add("todo-item");
         todoItem.innerHTML = `<input type="checkbox" ${todo.completed ? "checked" : ""}>
-        <span class="task-text">${todo.text}</span>
+        <span class="todo-text">${todo.text}</span>
         <div>
-            <button type="button" class="edit-task-btn">🖋️</button>
-            <button type="button" class="delete-task-btn">🗑️</button>
+            <button type="button" class="edit-todo-btn">🖋️</button>
+            <button type="button" class="delete-todo-btn">🗑️</button>
         </div>`;
         todoContainer.appendChild(todoItem);
     });
 };
 
 renderTodos();
+
+const todoInput = document.querySelector("#add-todo-input");
+const addTodoBtn = document.querySelector("#add-todo-btn");
+addTodoBtn.addEventListener("click", addTodo);
+todoInput.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        addTodo();
+    }
+});
+
+
+function addTodo () {
+    const text = todoInput.value.trim()
+    if (text) {
+            const newTodo = {
+                id: Date.now(),
+                text: text,
+                completed: false
+            };
+            todoList.push(newTodo);
+            renderTodos();
+            todoInput.value = "";
+    };
+};
+
+const deleteTodoBtn = document.querySelector(".delete-todo-btn");
+deleteTodoBtn.addEventListener("click", deleteTodo);
+
+
+function deleteTodo () {
+};
+
+
+
