a2e6d034476a13797e7d8209999a5c272b7542a8
a01034972144-hash <a01034972144@gmail.com>
2026-03-22 23:06:01 +0900

    feat: 'html 공부하기'를 리스트에 추가하고 확인하기(post)

--- test.js
+++ test.js
@@ -3,3 +3,18 @@ fetch("https://69bfedb972ca04f3bcba1290.mockapi.io/todos")
   .then(data => {
     console.log(data);
   })
+
+fetch("https://69bfedb972ca04f3bcba1290.mockapi.io/todos", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    title: "html 공부하기",
+    completed: false
+  })
+})
+  .then(response => response.json())
+  .then(newTodo => {
+    console.log(newTodo);
+  });
