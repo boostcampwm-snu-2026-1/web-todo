fd80432796329dd554e02fb64d9e9cd4fec17d7a
mgahn0706 <mgahn0706@snu.ac.kr>
2026-03-28 03:24:40 +0900

    feat: add test response

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.json({ Hello: "world" });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
