811d4f78e082edb9e93c71d6c9ec8f13ed5e8bac
snuzeus <cypark1213@snu.ac.kr>
2026-03-27 14:46:36 +0900

    feat: Express 서버 미들웨어 설정 및 Todo 조회 API(GET) 구현

--- server/backend.js
+++ server/backend.js
@@ -1,20 +1,36 @@
 import mongoose from 'mongoose';
+import express from 'express';
+import cors from 'cors'; 
+import 'dotenv/config';
+
+const app = express();
+app.use(cors()); 
+app.use(express.json()); 
 
 const todoSchema = new mongoose.Schema({
-  content: {
-    type: String,
-    required: true
-  },
-  isDone: {
-    type: Boolean,
-    default: false
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+  content: { type: String, required: true },
+  isDone: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now }
 });
 
 const Todo = mongoose.model('Todo', todoSchema);
+
+mongoose.connect(process.env.URI)
+  .then(() => console.log("✅ MongoDB 연결 성공!"))
+  .catch(err => console.error("❌ MongoDB 연결 실패:", err));
+
+app.get('/todos', async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.status(200).json(todos);
+  } catch (error) {
+    res.status(500).json({ message: "데이터 조회 실패", error })
+  }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 서버가 http://localhost:${PORT} 에서 달리는 중!`);
+});
 
 export default Todo;
