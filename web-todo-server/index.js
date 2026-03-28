2d57c6d0851e86050deff76c32c47af238b6c5ed
Jaewan Park <pona4321@snu.ac.kr>
2026-03-28 14:26:02 +0900

    chore: add dotenv, mongoose dependencies

--- web-todo-server/index.js
+++ web-todo-server/index.js
@@ -1,9 +1,9 @@
 import express from "express";
 import cors from "cors";
-const app = express()
-const port = 3000
 
+const app = express()
 app.use(cors());
+const port = 3000
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
