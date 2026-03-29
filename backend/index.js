0219bf6d79163843732ef6a48ab77d28921894cb
양해울 <haeul1005@naver.com>
2026-03-29 23:10:51 +0900

    index.js 리팩토링, 모듈화

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config";
+import todoRoutes from "./routes/todoRoutes.js";
 
 const app = express();
 app.use(cors());
@@ -15,69 +16,8 @@ mongoose.connect(URI)
   .then(async () => console.log("MongoDB Connected..."))
   .catch(err => console.log(err));
 
-// todo 스키마
-const todoSchema = new mongoose.Schema({
-    content: { type: String, required: true },
-    done: { type: Boolean, default: false },
-});
-
-// id라는 가상의 컬럼 추가. JSON 타입으로 변경할 때 _id 값을 그대로 보여줌
-todoSchema.virtual('id').get(function() {
-  return this._id.toHexString();
-}); 
-todoSchema.set('toJSON', { virtuals: true }) 
-
-const Todos = mongoose.model("Todos", todoSchema);
-
-
-app.get('/api/todos', async (req, res) => {
-    try {
-        const todos = await Todos.find();
-        res.status(200).json(todos);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-app.post('/api/todos', async (req, res) => {
-    try {
-        // 요청으로 받은 데이터를 이용하여 새로운 todo 생성
-        const newTodo = new Todos(req.body);
-
-        // DB에 데이터 저장
-        await newTodo.save();
-        
-        // 새로 생성된 사용자 정보를 응답
-        res.status(201).json(newTodo);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-app.put('/api/todos/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { done } = req.body;
-        const updatedTodo = await Todos.findByIdAndUpdate(id, { done });
-
-        if (!updatedTodo) return res.status(404).json({ message: "대상을 찾을 수 없습니다." });
-        res.status(200).json(updatedTodo);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-app.delete('/api/todos/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const deletedTodo = await Todos.findByIdAndDelete(id);
-
-        if (!deletedTodo) return res.status(404).json({ message: "대상을 찾을 수 없습니다." });
-        res.status(200).json({ id: deletedTodo._id });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
+// 라우터 연결
+app.use('/api/todos', todoRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
